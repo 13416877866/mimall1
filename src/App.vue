3b65066145184d152c59ea6115aa0f0c21c9fbf5
sunyao <2638706597@qq.com>
2021-09-19 19:02:21 +0800

    feat:增加注释

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{age}}
   </div>
 </template>
 
@@ -12,6 +13,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      age:30
+    }
   }
 }
 </script>
