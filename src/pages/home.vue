bfee93322787d5038067e012a3b4b3588c413e06
13416877866 <2638706597@qq.com>
2021-09-21 14:42:27 +0800

    4-7路由封装

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,16 +1,19 @@
-
 <template>
     <div>
+        home
         <nav-header></nav-header>
         <router-view></router-view>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
-import NavFooter from '../components/NavFooter.vue'
-import NavHeader from '../components/NavHeader.vue'
+import NavFooter from './../components/NavFooter.vue'
+import NavHeader from './../components/NavHeader.vue'
 export default{
-  components: { NavHeader, NavFooter },
-    name:'nav-header'
+    name:'nav-home',
+    components:{
+        NavHeader,
+        NavFooter
+    }
 }
 </script>
--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,16 +1,18 @@
-
 <template>
     <div>
-        <nav-header></nav-header>
+        <order-header></order-header>
         <router-view></router-view>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
-import NavFooter from '../components/NavFooter.vue'
-import NavHeader from '../components/NavHeader.vue'
+import OrderHeader from './../components/OrderHeader.vue'
+import NavFooter from './../components/NavFooter.vue'
 export default{
-  components: { NavHeader, NavFooter },
-    name:'nav-header'
+    name:'order',
+    components:{
+        OrderHeader,
+        NavFooter
+    }
 }
 </script>
