d82973b09f7dd9b0f7bdcdb96a3d3241fc7e55b7
13416877866 <2638706597@qq.com>
2021-09-21 10:09:18 +0800

    项目目录结构设置

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    {{age}}
+    <router-view></router-view>
   </div>  
 </template>
 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-
+import jsonp from 'jsonp'
 //vue-cli创建项目 mi-mall
 //托管平台建立仓库 mall
 //mi-mall git add. git commit -m '' git push ---> mall
 //mi-mall开发 git add. git commit -m '' git push ---> mall
-
 //git clone 仓库mall ---> 本地
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   },
   data(){
     return {
-      age:30
+     
     }
   },
   mounted(){
-    let url="https://www.imooc.com/common/adver-getadverlistbymarking"
-    axios.get(url).then(()=>{
-      
-    })
-  }
+   
+   }
 }
 </script>
 
