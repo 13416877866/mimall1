def10d5f4c987c3250e44d3ef20f839651d48600
13416877866 <2638706597@qq.com>
2021-09-29 22:04:29 +0800

    5-11

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -57,8 +57,16 @@
                 <div class="swiper-button-next" slot="button-next"></div>
             </swiper>
           </div>
-          <div class="ads-box"></div>
-          <div class="banner"></div>
+          <div class="ads-box">
+              <a v-bind:href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+                    <img v-bind:src="item.img" alt="">
+              </a>
+          </div>
+          <div class="banner">
+                <a href="/#/product/30">
+                    <img src="/imgs/banner-1.png" alt="">
+              </a>
+          </div>
           <div class="product-box"></div>
       </div>
       <service-bar></service-bar>
@@ -141,6 +149,21 @@ export default{
                  }
                 ],
                 [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+            ],
+            adsList:[
+                {
+                    id:33,
+                    img:'/imgs/ads/ads-1.png'
+                },{
+                    id:48,
+                    img:'/imgs/ads/ads-2.jpg'
+                },{
+                    id:45,
+                    img:'/imgs/ads/ads-3.png'
+                }, {
+                    id:47,
+                    img:'/imgs/ads/ads-4.jpg'
+                },
             ]
         }
     }
@@ -151,6 +174,7 @@ export default{
 <style lang="scss">
 @import './../assets/scss/config.scss';
 @import './../assets/scss/mixin.scss';
+@import './../assets/scss/base.scss';
 @media screen and (min-width:1226px){
     body{
         overflow-x:hidden;
@@ -238,6 +262,19 @@ export default{
                height: 100%;
            }
        }
-   }        
+   } 
+   .ads-box{
+       @include flex();
+       margin-top: 14px;
+       margin-bottom: 31px;
+       a{
+           width: 296px;
+           height: 167px;
+       }
+
+   } 
+   .banner{
+       margin-bottom: 50px;
+   }      
 }
 </style>
