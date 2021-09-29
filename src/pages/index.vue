589878d32bdb2e004b88ebb64c711a410f45f9cb
13416877866 <2638706597@qq.com>
2021-09-29 20:09:19 +0800

    5-9

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,42 @@
     <div class="index">
       <div class="container">
           <div class="swiper-box">
+              <div class="nav-menu">
+                <ul class="menu-wrap">
+                    <li class="menu-item">
+                        <a href="javascript:;">手机 电话卡</a>
+                        <div class="children"></div>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">电视 盒子</a>
+                        <div class="children"></div>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:;">笔记本 平板</a>
+                        <div class="children"></div>
+                    </li>
+                     <li class="menu-item">
+                        <a href="javascript:;">家电 插线板</a>
+                        <div class="children"></div>
+                    </li>
+                     <li class="menu-item">
+                        <a href="javascript:;">出行 穿戴</a>
+                        <div class="children"></div>
+                    </li>
+                     <li class="menu-item">
+                        <a href="javascript:;">智能 路由器</a>
+                        <div class="children"></div>
+                    </li>
+                     <li class="menu-item">
+                        <a href="javascript:;">电源 配件</a>
+                        <div class="children"></div>
+                    </li>
+                     <li class="menu-item">
+                        <a href="javascript:;">生活 箱包</a>
+                        <div class="children"></div>
+                    </li>
+                </ul>
+            </div>
             <swiper v-bind:options="swiperOption">
                 <swiper-slide v-for="(item,index) in slideList" v-bind:key="index">
                    <a V-bind:href="'/#/product/'+item.id"><img v-bind:src="item.img" alt=""></a>
@@ -79,18 +115,55 @@ export default{
 }
 </script>
 <style lang="scss">
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
 @media screen and (min-width:1226px){
     body{
         overflow-x:hidden;
     }
-}
+};
 .index{
    .swiper-box{
+       .nav-menu{
+           position: absolute;
+           width: 264px;
+           height: 451px;
+           z-index: 9;
+           padding: 26px 0;
+           background-color:#55585a63;
+           box-sizing: border-box;
+           .menu-wrap{
+               .menu-item{
+                   height: 50px;
+                   line-height: 50px;
+                   a{   
+                       position: relative;
+                       font-size: 16px;
+                       color: #ffffff;
+                       padding-left: 30px;
+                       display: block;
+                       &:after{
+                          position: absolute;
+                          right: 30px;
+                          top:17.5px;
+                          content:'';
+                          @include bgImg(10px,15px,'/imgs/icon-arrow.png');
+                       }
+                   }
+                   &:hover{
+                      background-color:$colorA ;
+                   }
+               }
+           }
+       }
        .swiper-container{
            height: 451px;
+           .swiper-button-prev{
+               left: 274px;
+           }
            img{
                width: 100%;
-               height:100% ;
+               height: 100%;
            }
        }
    }        
