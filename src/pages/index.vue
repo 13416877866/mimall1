bf8a3545693cdadd264a7f420cec87eb862eb6ab
13416877866 <2638706597@qq.com>
2021-09-30 12:04:59 +0800

    5-12

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -67,8 +67,34 @@
                     <img src="/imgs/banner-1.png" alt="">
               </a>
           </div>
-          <div class="product-box"></div>
       </div>
+      <div class="product-box">
+              <div class="container">
+                  <h2>手机</h2>
+              <div class="wrapper">
+                  <div class="banner-left">
+                      <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+                  </div>
+                  <div class="list-box">
+                      <div class="list" v-for="(arr,i) in phoneList" :key="i">
+                          <div class="item" v-for="(item,j) in arr" :key="j">
+                         <span>新品</span>
+                         <div class="item-img">
+                             <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c5c752522100ecd364cc53752bb660bc.jpg?thumb=1&w=250&h=250&f=webp&q=90" alt="">
+                         </div>
+                         <div class="item-info">
+                             <h3>小米9</h3>
+                             <p>骁龙855，索尼4800万超广角微距</p>
+                             <p class="price">2999元</p>
+                         </div>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+
+              </div>
+             
+          </div>
       <service-bar></service-bar>
     </div>
 </template>
@@ -164,7 +190,8 @@ export default{
                     id:47,
                     img:'/imgs/ads/ads-4.jpg'
                 },
-            ]
+            ],
+            phoneList:[[1,1,1,1],[1,1,1,1]]
         }
     }
     
@@ -275,6 +302,80 @@ export default{
    } 
    .banner{
        margin-bottom: 50px;
-   }      
+   } 
+   .product-box{
+       background-color: $colorJ;
+       padding: 30px 0 50px;
+       h2{
+           font-size: 22px;
+           height: 21px;
+           line-height: 21px;
+           color: $colorB;
+           margin-bottom: 20px;
+       }
+       .wrapper{
+           display: flex;
+        //    margin-left: 16px;
+           .banner-left{
+               img{
+                   width: 224px;
+                   height: 619px;
+               }
+           }
+           .list-box{
+               .list{
+                   @include flex();
+                   width: 986px;
+                   margin-bottom: 14px;
+                   &:last-child{
+                       margin-bottom: 0;
+                   }
+                   .item{
+                       width: 236px;
+                       height: 302px;
+                       background-color: #ffffff;
+                       text-align: center;
+                       span{
+
+                       }
+                       .item-img{
+                           img{
+                               height: 195px;
+                           }
+                       }
+                       .item-info{
+                           h3{
+                               font-size: 14px;
+                               color: #333333;
+                               line-height: 14px;
+                               font-weight: bold;
+                           }
+                           p{
+                               color: #999999;
+                               line-height: 13px;
+                               margin: 6px auto 13px;
+                           }
+                           .price{
+                               color: #f20a0a;
+                               font-size: 14px;
+                               font-weight: bold;
+                               cursor:pointer;
+                               &:after{
+                                   @include bgImg(22px,22px,'/imgs/icon-cart-hover.png');
+                                   content: ' ';
+                                   margin-left: 5px;
+                                   vertical-align:middle;
+                               }
+                           }
+
+                       }
+                   }
+
+               }
+           }
+
+       }
+
+   }     
 }
 </style>
